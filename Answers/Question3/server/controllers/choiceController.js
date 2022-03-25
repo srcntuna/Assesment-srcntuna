62254f34aa296a04316655e449930764b883e757
Sercan Tuna <ssercantuna@hotmail.com>
2022-03-24 23:43:35 -0400

    update to choiceController

--- Answers/Question3/server/controllers/choiceController.js
+++ Answers/Question3/server/controllers/choiceController.js
@@ -6,13 +6,12 @@ choiceController.postChoices = async (req, res, next) => {
   const { choices } = req.body;
   const [choiceA, choiceB, choiceC] = choices;
 
-  console.log(choices);
-
   console.log('server choices', choices);
 
   if (!choices.join(' ').includes('calculus')) return next(err);
 
-  const q = 'INSERT INTO Messages (choiceA,choiceB, choiceC) VALUES ($1,$2,$3)';
+  //sending DATA to DB to save
+  const q = 'INSERT INTO Choices (choiceA,choiceB, choiceC) VALUES ($1,$2,$3)';
 
   await db.query(q, [choiceA, choiceB, choiceC], (err, result) => {
     if (err) {
